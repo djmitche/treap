277c69fc0785f1ca74a7bb39e191d572df1039b0
Dustin J. Mitchell <dustin@v.igoro.us>
2024-06-05 22:24:54 -0400

    deletion

--- src/lib.rs
+++ src/lib.rs
@@ -111,19 +111,73 @@ impl Treap {
         self.root = Some(insert(&mut self.nodes, self.root, a, u));
     }
 
+    /// Delete the node with first coordinate `a` from the Treap (O(log(n))).
+    pub fn delete(&mut self, a: u64) {
+        /// Return the index of the root of a subtree containing all children of the given node,
+        /// but not the node itself.
+        fn delete(nodes: &mut [Node], idx: usize) -> Option<usize> {
+            let n = &nodes[idx];
+            match (n.l, n.r) {
+                (None, None) => None,
+                (Some(l), None) => Some(l),
+                (None, Some(r)) => Some(r),
+                (Some(l), Some(r)) => {
+                    // Choose one of l or r to be the new root; then the opposite
+                    // beomes one of its children. The other child is the tree resulting
+                    // from deleting the new root.
+                    if nodes[l].u > nodes[r].u {
+                        // l is the new root, and r will be its right child.
+                        let new_child = delete(nodes, l);
+                        nodes[l].l = new_child;
+                        nodes[l].r = Some(r);
+                        Some(l)
+                    } else {
+                        // r is the new root, and l will be its left child
+                        let new_child = delete(nodes, r);
+                        nodes[r].l = Some(l);
+                        nodes[r].r = new_child;
+                        Some(r)
+                    }
+                }
+            }
+        }
+
+        /// Find the node with the given `a` in the given subtree and delete it, returning the new
+        /// subtree.
+        fn find_and_delete(nodes: &mut [Node], idx: Option<usize>, a: u64) -> Option<usize> {
+            let Some(idx) = idx else {
+                return None;
+            };
+            // TODO: keep a free list and add the deleted node to it
+            match a.cmp(&nodes[idx].a) {
+                std::cmp::Ordering::Less => {
+                    nodes[idx].l = find_and_delete(nodes, nodes[idx].l, a);
+                    Some(idx)
+                }
+                std::cmp::Ordering::Equal => delete(nodes, idx),
+                std::cmp::Ordering::Greater => {
+                    nodes[idx].r = find_and_delete(nodes, nodes[idx].r, a);
+                    Some(idx)
+                }
+            }
+        }
+
+        self.root = find_and_delete(&mut self.nodes, self.root, a);
+    }
+
     /// Find a pair with the given `a`, and return its `u`, if any (O(log n)).
-    pub fn find_a(&self, a: u64) -> Option<u64> {
-        fn find_a(nodes: &[Node], idx: Option<usize>, a: u64) -> Option<u64> {
+    pub fn lookup(&self, a: u64) -> Option<u64> {
+        fn lookup(nodes: &[Node], idx: Option<usize>, a: u64) -> Option<u64> {
             let Some(idx) = idx else {
                 return None;
             };
             match a.cmp(&nodes[idx].a) {
-                std::cmp::Ordering::Less => find_a(nodes, nodes[idx].l, a),
+                std::cmp::Ordering::Less => lookup(nodes, nodes[idx].l, a),
                 std::cmp::Ordering::Equal => Some(nodes[idx].u),
-                std::cmp::Ordering::Greater => find_a(nodes, nodes[idx].r, a),
+                std::cmp::Ordering::Greater => lookup(nodes, nodes[idx].r, a),
             }
         }
-        find_a(&self.nodes, self.root, a)
+        lookup(&self.nodes, self.root, a)
     }
 
     /// Get the maximum value of `u` in any pair (O(1)).
@@ -162,10 +216,12 @@ mod tests {
 
     #[test]
     fn empty_treap() {
-        let t = Treap::new();
+        let mut t = Treap::new();
         assert_eq!(t.len(), 0);
-        assert_eq!(t.find_a(10), None);
+        assert_eq!(t.lookup(10), None);
         assert_eq!(t.peek_max_u(), None);
+        t.delete(99);
+        assert_eq!(t.len(), 0);
     }
 
     fn build_and_check_no_dup(vals: &[(u64, u64)]) -> Treap {
@@ -181,7 +237,7 @@ mod tests {
         dbg!(&t).check();
         assert_eq!(t.len(), vals.len());
         for (a, u) in vals {
-            assert_eq!(t.find_a(*a), Some(*u));
+            assert_eq!(t.lookup(*a), Some(*u));
         }
         t
     }
@@ -203,7 +259,19 @@ mod tests {
 
     #[test]
     fn mixed_treap() {
-        let t = build_and_check_no_dup(&[(3, 10), (9, 80), (2, 30), (1, 40), (8, 50), (4, 60)]);
-        assert_eq!(t.peek_max_u(), Some((9, 80)))
+        let mut t = build_and_check_no_dup(&[(3, 10), (9, 80), (2, 30), (1, 40), (8, 50), (4, 60)]);
+        assert_eq!(t.peek_max_u(), Some((9, 80)));
+
+        t.delete(9);
+        assert_eq!(t.lookup(9), None);
+        assert_eq!(t.peek_max_u(), Some((4, 60)));
+
+        t.delete(3);
+        assert_eq!(t.lookup(3), None);
+        assert_eq!(t.peek_max_u(), Some((4, 60)));
+
+        t.delete(8);
+        assert_eq!(t.lookup(8), None);
+        assert_eq!(t.peek_max_u(), Some((4, 60)));
     }
 }
